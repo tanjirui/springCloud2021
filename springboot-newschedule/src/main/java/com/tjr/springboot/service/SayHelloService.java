eb0226032fa0982ddccf8dcdb260b70c4e59381e
tanjirui <351153723@qq.com>
2021-05-20 17:05:47 +0800

    Update SayHelloService.java

--- springboot-newschedule/src/main/java/com/tjr/springboot/service/SayHelloService.java
+++ springboot-newschedule/src/main/java/com/tjr/springboot/service/SayHelloService.java
@@ -22,4 +22,8 @@ public class SayHelloService {
     public void likeYou(){
         System.out.println("I Like You");
     }
+    
+    public void haha520(){
+    System.out.println("haha520");
+    }
 }
